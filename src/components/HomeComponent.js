45bddd382811a73409f4ff0a176b554772864468
DEV_1 <dev1@oceantech.vn>
2022-02-17 12:18:58 +0700

    add feature display tag

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -11,10 +11,11 @@ export default class Home extends Component {
 
     this.state = {
       content: [],
-      tags: []
+      tags: [],
+      post_tags: []
     };
   }
-  
+
 
   componentDidMount() {
     UserService.getPublicContent().then(
@@ -22,22 +23,26 @@ export default class Home extends Component {
         response.data.forEach(element => {
           PostService.getPostwithTag(element.id).then(
             response => {
-              element.tags = response.data["tags"];
+              element.post_tags = response.data["tags"];
+              this.setState({
+                post_tags: response.data["tags"],
+              });
+              // console.log(this.state.post_tags);
             }
           )
         });
         this.setState({
           content: response.data
-         
+
         });
       },
-        PostService.getTopTags().then(
-          response => {
-            this.setState({
-              tags: response.data
-            });
-          }
-        ),
+      PostService.getTopTags().then(
+        response => {
+          this.setState({
+            tags: response.data
+          });
+        }
+      ),
       error => {
         this.setState({
           content:
@@ -48,45 +53,46 @@ export default class Home extends Component {
       }
     );
   }
-  
+
   handlePost = (slug) => {
     PostService.getSlug(slug).then(response => {
       alert(response.data.content);
     })
   }
   render() {
-    console.log(this.state.content);
-    console.log(this.state.tags);
+    // console.log(this.state.content);
+    // console.log(this.state.tags);
+    console.log(this.state.post_tags);
     return (
       <Container>
         <Row>
           <Col sm={9}>
             {this.state.content.map(item => {
               return <div key={item.id} className="home-container">
-              <div className="course">
-                <div className="course-preview">
-                <img src={item.thumbnail} alt="none" class="img-fluid"></img>
-                </div>
-                <div className="course-info">
-                  <h3>{item.title}</h3>
-                  <h6>{item.brief}</h6>
-                  <h6>TAG: {item.tags}</h6>
-                  <h6>Date: {item.createdAt}</h6>
-                  <button className="btn" onClick={() => this.handlePost(item.slug)}>Read</button>
+                <div className="course">
+                  <div className="course-preview">
+                    <img src={item.thumbnail} alt="none" className="img-fluid"></img>
+                  </div>
+                  <div className="course-info">
+                    <h3>{item.title}</h3>
+                    <h6>{item.brief}</h6>
+                    <h6>TAG: {this.state.post_tags}</h6>
+                    <h6>Date: {item.createdAt}</h6>
+                    <button className="btn" onClick={() => this.handlePost(item.slug)}>Read</button>
+                  </div>
                 </div>
               </div>
-            </div>
             })}
           </Col>
           <Col sm={3}>
-          <div className="home-container">
-          {this.state.tags.map(item => {
-              return <div className="course">
-                <div className="course-info">
-                  <h6>{item.name} ({item.count})</h6>
+            <div className="home-container">
+              {this.state.tags.map(item => {
+                return <div key={item.name} className="course">
+                  <div className="course-info">
+                    <h6>{item.name} ({item.count})</h6>
+                  </div>
                 </div>
-              </div>
-            })}
+              })}
             </div>
           </Col>
         </Row>
